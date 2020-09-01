2286ef3c33f32a3943f163d46175214a2711db2c
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-01 16:00:42 -0600

    Added CharacterMover script

---
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message: "Hello World!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = speed*Input.GetAxis("Horizontal")*Time.deltaTime;
+        y = speed*Input.GetAxis("Vertical")*Time.deltaTime;
+        transform.Translate(x,y,z);
+    }
+}

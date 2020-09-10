bbdd7736b01ec0aef1d10f39bfbb38c67f2eeb65
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-10 17:38:25 -0600

    Added HealthPowerUp

---
+++ Pickle Pot/Assets/Scripts/HealthPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    public int health;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health++;
+        print(health);
+    }
+}
+

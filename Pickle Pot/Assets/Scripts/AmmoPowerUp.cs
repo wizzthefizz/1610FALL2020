662affdc12114e1eac968829b18840f976ccad0f
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-11 15:00:22 -0600

    Added AmmoPowerUp

---
+++ Pickle Pot/Assets/Scripts/AmmoPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPowerUp : MonoBehaviour
+{
+    public int ammo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+        print(ammo);
+    }
+}
+

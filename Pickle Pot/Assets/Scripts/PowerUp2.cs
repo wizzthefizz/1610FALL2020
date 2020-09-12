bb096e0c3666ff73ae81deb57464e60a4a986316
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-12 09:17:22 -0600

    Added additional powerup

---
+++ Pickle Pot/Assets/Scripts/PowerUp2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp2 : MonoBehaviour
+{
+   public int powerup;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      powerup++;
+      print(powerup);
+   }
+}

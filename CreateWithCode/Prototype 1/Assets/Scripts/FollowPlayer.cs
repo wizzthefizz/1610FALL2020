5a0eb9aab1125f259d3b3c68e12fd714b6ccef83
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-20 18:41:18 -0600

    Completed Unit 1 Create with code

--- CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0,5,-7);
+    private Vector3 offset = new Vector3(0, 5, -7);
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Late Update is called after each Update frame
     void Update()
     {
         transform.position = player.transform.position + offset;
--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
 
-    private float turnSpeed = 45.0f;
-
+    public float speed = 40.0f;
+    public float turnSpeed = 45.0f;
     private float horizontalInput;
-
     private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Fixed Update is called once per physics frame
+    void FixedUpdate()
     {
-        // Move the vehicle 
         horizontalInput = Input.GetAxis("Horizontal");
+
+
         forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward);
+        transform.Translate(1, 0, 0);
+
         // Moves the car forward based on vertical input
-        
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
+
     }
 }

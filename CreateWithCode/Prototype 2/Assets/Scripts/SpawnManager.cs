ad42be787e2bfa9c75eae792bbae779449ff80b4
Emmorie Decker <decker.emmorie@gmail.com>
2020-09-29 20:33:23 -0600

    Create with code collision decisions

--- CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -6,25 +6,28 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20f;
-    private float spawnPosZ = 20;
-    
+    private float spawnPosZ = 20f;
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+    }
+
+    void SpawnRandomAnimal()
         {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(- spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
         }
 
     }
-}

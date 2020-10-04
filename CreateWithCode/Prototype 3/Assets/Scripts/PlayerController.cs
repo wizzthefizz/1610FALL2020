00efadd9c780ac89822dd7c2a3a14e02c83bc0f3
Emmorie Decker <decker.emmorie@gmail.com>
2020-10-04 13:18:33 -0600

    Create with code "Don't just stand there"

--- CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -12,10 +12,15 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
 
     public bool isOnGround = true;
+
+    public bool gameOver = false;
+
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
 
     }
@@ -23,15 +28,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int",1);
+        }
+
     }
-}
+    }

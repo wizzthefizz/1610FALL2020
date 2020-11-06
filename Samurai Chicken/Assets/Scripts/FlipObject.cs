0f9904913c49a21513f693dd441e3dc05287d192
Emmorie Decker <decker.emmorie@gmail.com>
2020-11-06 12:15:06 -0700

    Added flip object script to personal game project

---
+++ Samurai Chicken/Assets/Scripts/FlipObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FlipObject : MonoBehaviour
+{
+    private float rotateValue;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rotateValue = 90;
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rotateValue = 270;
+        }
+        
+        transform.rotation = Quaternion.Euler(0,rotateValue,0);
+    }
+}
